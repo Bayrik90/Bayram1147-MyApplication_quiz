75a37c6a6f53e0fb778c1319178e34e9a549bd40
Aslanov Bayram <bayram.aslanov.90@mail.ru>
2021-10-06 13:29:49 +0300

    Проблема при повороте экрана(сбрасывается вопрос)

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button yesBtn;//1
+    Button noBtn;//4
+    TextView textView;
+    Question[] questions = {
+            new Question(R.string.question1, true),
+            new Question(R.string.question2, true),
+            new Question(R.string.question3, true),
+            new Question(R.string.question4, false),
+            new Question(R.string.question5, false)
+    };
+    int questionIndex = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        yesBtn=findViewById(R.id.yesBtn);//2 R-это папка ресурс,в кот находим id и через него выходим на на йесбтн
+        noBtn=findViewById(R.id.noBtn);//5
+        textView = findViewById(R.id.textView);
+        textView.setText(questions[questionIndex].getQuestionText());
+        yesBtn.setOnClickListener(new View.OnClickListener() {//3 при клике будет появ-ся след метод:
+            @Override
+            public void onClick(View view) {//3 показ уведомления системы
+                checkAnswer(true);
+            }
+        });
+        noBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(false);
+            }
+        });
+    }
+    public void checkAnswer(boolean btn){
+        if((questions[questionIndex].isAnswerTrue() && btn) || (!questions[questionIndex].isAnswerTrue() && !btn))
+            Toast.makeText(MainActivity.this, R.string.correct_answer, Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(MainActivity.this, R.string.incorrect_answer, Toast.LENGTH_SHORT).show();
+        questionIndex = (questionIndex+1)%questions.length;
+        textView.setText(questions[questionIndex].getQuestionText());
+    }
+}
